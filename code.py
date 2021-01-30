d2b884ce8610d90706596ad45d4ed78216ff2de3
Kartik Panchal <46402857+kartikpanchal2000@users.noreply.github.com>
2021-01-30 20:27:30 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+st="aabasgdhbc"
+count=0
+st=sorted(st)
+for i in range(0,len(st)-1):
+    if(st[i]!=st[i+1] and i==0):
+        count+=1
+    elif(st[i]!=st[i+1] and st[i-1]!=st[i]):
+        count+=1
+    elif(i==len(st) and st[i-1]!=st[i]):
+        count+=1
+print(count)
+        
